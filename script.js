7617faf23babffe9a38173a1deb2e738fc93fdbb
ZetaCraftSMP <159169219+ZetaCraftSMP@users.noreply.github.com>
2024-02-06 17:33:45 +0100

    The start of the site maybne

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript code for your website
+
+// Example function to handle button click event
+function handleClick() {
+    alert('Button clicked!');
+}
+
+// Example function to change text color on mouseover
+function changeTextColor() {
+    document.getElementById('text').style.color = 'red';
+}
+
+// Example function to change text color back to black on mouseout
+function resetTextColor() {
+    document.getElementById('text').style.color = 'black';
+}
